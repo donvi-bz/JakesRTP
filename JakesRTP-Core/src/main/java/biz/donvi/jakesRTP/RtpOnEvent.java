2c38d1dc4c57c43fbfe6962b429e13a2e1240f2f
JakeTS <timpbh@gmail.com>
2021-01-24 15:24:11 -0500

    RtpOnEvent debug info

--- JakesRTP-Core/src/main/java/biz/donvi/jakesRTP/RtpOnEvent.java
+++ JakesRTP-Core/src/main/java/biz/donvi/jakesRTP/RtpOnEvent.java
@@ -1,16 +1,25 @@
 package biz.donvi.jakesRTP;
 
+import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.plugin.RegisteredListener;
+
+import java.util.WeakHashMap;
+import java.util.logging.Level;
 
 public class RtpOnEvent implements Listener {
 
     private final RandomTeleporter randomTeleporter;
 
+    private final WeakHashMap<PlayerJoinEvent, Location>    oldJoinEvents    = new WeakHashMap<>();
+    private final WeakHashMap<PlayerRespawnEvent, Location> oldRespawnEvents = new WeakHashMap<>();
+
     public RtpOnEvent(RandomTeleporter randomTeleporter) {this.randomTeleporter = randomTeleporter;}
+
 
     /**
      * When {@code firstJoinRtp} is enabled, this will RTP a player when they join the server
@@ -18,22 +27,31 @@ public class RtpOnEvent implements Listener {
      *
      * @param event The PlayerJoinEvent
      */
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void playerJoin(PlayerJoinEvent event) {
         if (!randomTeleporter.firstJoinRtp || event.getPlayer().hasPlayedBefore()) return;
         try {
             assert randomTeleporter.firstJoinSettings != null;
             assert randomTeleporter.firstJoinSettings.landingWorld != null;
-            new RandomTeleportAction(
+            RandomTeleportAction rtpa = new RandomTeleportAction(
                 randomTeleporter,
                 randomTeleporter.firstJoinSettings,
                 randomTeleporter.firstJoinSettings.landingWorld.getSpawnLocation(),
                 true, true,
                 randomTeleporter.logRtpOnPlayerJoin, "Rtp-on-join triggered!"
             ).teleportSync(event.getPlayer());
+            oldJoinEvents.put(event, rtpa.getLandingLoc().clone());
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void playerJoinMonitor(PlayerJoinEvent event) {
+        Location properLoc = oldJoinEvents.get(event);
+        Location actualLoc = event.getPlayer().getLocation();
+        if (properLoc == null || locAlmostEqual(properLoc, actualLoc)) return;
+        handlerLogging(event.getHandlers().getRegisteredListeners(), "rtp-on-join", "PlayerJoinEvent");
     }
 
 
@@ -46,24 +64,59 @@ public class RtpOnEvent implements Listener {
      *
      * @param event
      */
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void playerRespawn(PlayerRespawnEvent event) {
         //All conditions must be met to continue
         if (randomTeleporter.onDeathRtp &&
             (!randomTeleporter.onDeathRequirePermission || event.getPlayer().hasPermission("jakesrtp.rtpondeath")) &&
             (!randomTeleporter.onDeathRespectBeds || !(event.isBedSpawn()/* || event.isAnchorSpawn()*/))
         ) try {
-            event.setRespawnLocation(
-                new RandomTeleportAction(
-                    randomTeleporter,
-                    randomTeleporter.onDeathSettings,
-                    event.getPlayer().getLocation(),
-                    true, true,
-                    randomTeleporter.logRtpOnRespawn, "Rtp-on-respawn triggered!"
-                ).requestLocation()
-            );
+            Location landingLoc = new RandomTeleportAction(
+                randomTeleporter,
+                randomTeleporter.onDeathSettings,
+                event.getPlayer().getLocation(),
+                true, true,
+                randomTeleporter.logRtpOnRespawn, "Rtp-on-respawn triggered!"
+            ).requestLocation();
+            event.setRespawnLocation(landingLoc);
+            oldRespawnEvents.put(event, landingLoc.clone());
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void playerRespawnMonitor(PlayerRespawnEvent event) {
+        Location properLoc = oldRespawnEvents.get(event);
+        Location actualLoc = event.getRespawnLocation();
+        if (properLoc == null || locAlmostEqual(properLoc, actualLoc)) return;
+        handlerLogging(event.getHandlers().getRegisteredListeners(), "rtp-on-death", "PlayerRespawnEvent");
+    }
+
+
+    private static void handlerLogging(RegisteredListener[] registeredListeners, String msg1, String msg2) {
+        int i = 0, us = 0;
+        JakesRtpPlugin.log(Level.WARNING, String.format(
+            "It looks like you have %s enabled, but some plugin is messing with the results!\n" +
+            "Here is a print out of every plugin that messes with the %s event, hopefully this helps.", msg1, msg2));
+        for (RegisteredListener listener : registeredListeners) {
+            String logMsg = String.format("%02d: [%s] %s", i++,
+                                          listener.getPriority().toString(),
+                                          listener.getPlugin().getName());
+            if (listener.getPlugin() instanceof JakesRtpPlugin) {
+                if (us == 0) {
+                    logMsg += " ~ Look between here ↓";
+                    us++;
+                } else if (us == 1) {
+                    logMsg += " ~ And here ↑";
+                    us++;
+                }
+            }
+            JakesRtpPlugin.infoLog(logMsg);
+        }
+    }
+
+    private static boolean locAlmostEqual(Location a, Location b) {
+        return a.getBlockX() == b.getBlockX() && a.getBlockY() == b.getBlockY() && a.getBlockZ() == b.getBlockZ();
     }
 }
